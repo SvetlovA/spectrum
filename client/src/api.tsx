55e7c175574d68114a5786ac32bff13b18799015
SvetlovA <21arte62@gmail.com>
2020-05-25 02:24:34 +0300

    added frontend app

---
+++ client/src/api.tsx
@@ -0,0 +1,13 @@
+export const processRequest = async (path: string, method: string, data: any) => {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let response = await fetch(path, {
+        headers,
+        method: method,
+        body: JSON.stringify(data),
+        credentials: 'include'
+    });
+
+    return await response.json();
+}
